7346c53c5be64cdc1dd0ed143dc08e53b430623d
Muhammad hussain <m.hussain7006@gmail.com>
2020-06-20 11:45:10 +0500

    Create UI, Context and Reducer
    Created all components and also created context and reducer and also wrap with provider

---
+++ src/components/AccountSummary.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const AccountSummary = () => {
+    return (
+        <div className="inc-exp-container">
+            <div>
+                <h4>Income</h4>
+                <p className="money-plus">
+                    $0.00
+                </p>
+            </div>
+            <div>
+                <h4>Expence</h4>
+                <p className="money-minus">
+                    $0.00
+                </p>
+
+            </div>
+        </div>
+    )
+}
+
+export default AccountSummary
---
+++ src/components/AddTransaction.js
@@ -0,0 +1,48 @@
+import React,{useState} from 'react'
+
+const AddTransaction = () => {
+
+    const [text,setText] = useState('');
+    const [amount,setAmount] = useState(0);
+
+
+    return (
+        <div>
+            <h3>Add New Transaction</h3>
+
+            <form>
+                <div className="form-control">
+                    <label htmlFor="description">
+                        Description
+                    </label>
+                    <input  type="text" 
+                            id="description"
+                            placeholder="Detail of Transaction"
+                            value = {text}
+                            onChange = {(e) => setText(e.target.value)}
+                    >
+                    </input>
+                    
+                </div>
+                <div className="form-control">
+                    <label htmlFor="transactionamount">
+                        Transaction Amount
+                    </label>
+                    <input  type="number"
+                            id="transactionamount"
+                            placeholder="Enter Transaction Amount"
+                            value = {amount}
+                            onChange = {(e) => setAmount(e.target.value)}
+                    >
+                    </input>
+
+                </div>
+                <button className="btn">
+                    Add Transaction
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default AddTransaction
